c7ad0c5111e6503990974104e5b58c8c1215c3a3
negociosHFmorales <negocios.HFmorales@gmail.com>
2025-07-30 20:28:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -814,4 +814,4 @@ def clear_cache():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port
+    app.run(host='0.0.0.0', port=port, debug=False)
